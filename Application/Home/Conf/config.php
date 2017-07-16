26a9400793cd7f3b7e9ed1f3c88317639403c33e
wheel <254232364@qq.com>
2017-07-17 00:04:09 +0800

    修改数据库文件

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -6,10 +6,10 @@ return array(
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  'demo.yuanxu.top',          // 数据库名
-    'DB_USER'               =>  'demo.yuanxu.top',      // 用户名
-    'DB_PWD'                =>  '8836dd5fb6d06f74',          // 密码
+    'DB_NAME'               =>  'tt',          // 数据库名
+    'DB_USER'               =>  'tt',      // 用户名
+    'DB_PWD'                =>  'tt',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  'yc_',    // 数据库表前缀
+    'DB_PREFIX'             =>  'tt_',    // 数据库表前缀
 
 );
